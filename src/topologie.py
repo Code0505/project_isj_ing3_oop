b79500a41ea5fc39ca15d154fc470943d8c26dca
Michel-Ange NOUMBO <michelangenoumbo6@gmail.com>
2026-05-22 23:27:17 +0100

    feat: ajout des docstrings

--- src/topologie.py
+++ src/topologie.py
@@ -1,5 +1,10 @@
 from equipements import Equipement
 class Lien: 
+    """
+
+    Représente le lien physique entre deux équipements.
+
+    """
     def __init__(self, equipement1, equipement2, bande_passante, latence):
         self.equipement1 = equipement1 
         self.equipement2 = equipement2
@@ -10,6 +15,11 @@ class Lien:
     def __str__(self):
         return f"{self.equipement1.nom} et {self.equipement2.nom}  Bande Passante : {self.bande_passante}Mbps {self.latence}"
 class Topologie:
+    """
+
+    Contiens l'ensemble des équipements et des liens et des équipements qui sont reliés l'un à l'autre.
+
+    """
     def __init__(self):
         self.equipements = {}
         self.liens = [] 
