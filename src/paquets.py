dd7a9cba533c3979d9ff910ff3c1bc641175d13d
tagbo-dot <taguiwaboris@gmail.com>
2026-05-22 11:11:43 +0100

    ajout de paquet

--- src/paquets.py
+++ src/paquets.py
@@ -15,3 +15,21 @@ class Paquet:
             f"Priorité = {self.priorite}, "
             f"Port = {self.port_destination})"
         )
+        class Paquet:
+    def __init__(self, source, destination, protocole, taille, priorite, port_destination):
+        self.source = source
+        self.destination = destination
+        self.protocole = protocole
+        self.taille = taille
+        self.priorite = priorite
+        self.port_destination = port_destination
+
+    def __str__(self):
+        return(
+            f"Paquet(Source = {self.source}, "
+            f"Destination = {self.destination}, "
+            f"Protocole = {self.protocole}, "
+            f"Taille = {self.taille} octets, "
+            f"Priorité = {self.priorite}, "
+            f"Port = {self.port_destination})"
+        )
